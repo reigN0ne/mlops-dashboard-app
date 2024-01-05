c0d4b89f602e70fb48ab0fd2bc10c563e07eead7
reigN0ne <python12345612@gmail.com>
2024-01-05 23:48:53 +0530

    Experimenting with metrics

--- algo/Regression/random_forest.py
+++ algo/Regression/random_forest.py
@@ -2,8 +2,9 @@ import mlflow
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 
-with mlflow.start_run():
+with mlflow.start_run() as run:
     db = load_diabetes()
     X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
 
@@ -14,8 +15,13 @@ with mlflow.start_run():
     mlflow.log_params({"n_estimators": 100, "max_depth":6, "max_features":3, "random_state": 42})
     mlflow.sklearn.log_model(rf, "model")
 
-    # Use the model to make predictions on the test dataset.
+    # Log metric
     predictions = rf.predict(X_test)
+    print("y_test ->", y_test)
+    rmse = mean_squared_error(y_test, predictions, squared=False)
+    mlflow.log_metric("rmse_score", rmse)
+    
+    # Use the model to make predictions on the test dataset.
     print(predictions)
 
 # def train(X_train, y_train, hyperparameters):
--- main.py
+++ main.py
@@ -1,47 +1,47 @@
 import streamlit as st
 import mlflow
 from mlflow.tracking import MlflowClient
+import json
 
-# Initialize Streamlit app
-st.title("MLflow MLOps App")
+st.title("MLOps Dashboard App")
 
-# Connect to MLflow
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 client = MlflowClient()
 
-# Sidebar for selecting runs
-st.sidebar.title("Select Run")
-print("****************")
-print(client.search_experiments())
-print("****************")
-run_id = st.sidebar.selectbox("Select run:", [run.experiment_id for run in client.search_experiments()])
-print(run_id)
+st.sidebar.title("All your experiments will appear here")
+experiment_id = st.sidebar.selectbox("Select Experiment:", [run.experiment_id for run in client.search_experiments()])
 
-# Display run details
-run = client.get_run(run_id)
-st.subheader(f"Run Details for {run.info.run_id}")
-st.write(f"Run Name: {run.data.tags['mlflow.runName']}")
-st.write(f"Status: {run.info.status}")
-st.write(f"Start Time: {run.info.start_time}")
-st.write(f"End Time: {run.info.end_time}")
-st.write(f"Artifact URI: {run.info.artifact_uri}")
+runs = mlflow.search_runs([experiment_id])
+params_col = [col.split(".")[-1] for col in runs.columns if col.startswith("params")]
 
-# Display metrics and parameters
-st.subheader("Metrics")
-metrics = client.get_metric_history(run.info.run_id)
-for metric in metrics:
-    st.write(f"{metric.key}: {metric.value}")
+for run_id in range(len(runs)):
+    with st.expander(f"Run Details for Tracker #{runs['run_id'][run_id]}", expanded=False):
+        st.subheader(f"Run Details")
+        
+        col_1, col_2 = st.columns(2)
+        
+        with col_1:
+            st.write(f"Run Name: {runs['tags.mlflow.runName'][run_id]}")
+            st.write(f"Status: {runs['status'][run_id]}")
+        
+        with col_2:
+            st.write(f"Start Time: {runs['start_time'][run_id]}")
+            st.write(f"End Time: {runs['end_time'][run_id]}")
 
-st.subheader("Parameters")
-params = client.get_run(run.info.run_id).data.params
-for param_key, param_value in params.items():
-    st.write(f"{param_key}: {param_value}")
-
-# Display model artifacts
-st.subheader("Model Artifacts")
-artifacts = mlflow.search_runs(f"run_id='{run.info.run_id}'")
-for _, artifact in artifacts.iterrows():
-    artifact_path = artifact['artifact_uri'] + artifact['path']
-    st.write(f"- {artifact_path}")
-
-# Add more sections as needed, such as deployment options, retraining, etc.
+        st.subheader("Track Parameters")
+        col_3, col_4 = st.columns(2)
+        for idx, param in enumerate(params_col):
+            if idx % 2 == 0:
+                with col_3:
+                    st.write(f"{param}: {runs['params.' + param][run_id]}")
+            else:
+                with col_4:
+                    st.write(f"{param}: {runs['params.' + param][run_id]}")
+            
+        st.subheader("Model Artifacts")
+        artifacts_data = json.loads(runs['tags.mlflow.log-model.history'][run_id])
+        artifacts_path = artifacts_data[0]['artifact_path']
+        model_path = artifacts_data[0]['flavors']['python_function']['model_path']
+        
+        st.write(f"Artifact Path: {runs['artifact_uri'][run_id]}" + "/" + artifacts_path)
+        st.write("Model Name: " + model_path)
